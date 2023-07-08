dd81f4dada8d76045fca90c17c039c591846d2b7
Chris Min <chrismin@Bu.edu>
2023-07-08 16:03:33 -0400

    Initialized react app

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      <header>
+        <a href="" className='Logo'>CMBlogs</a>
+        <nav>
+          <a href="">Login</a>
+          <a href="">Register</a>
+        </nav>
+      </header>
+    </main>
+  );
+}
+
+export default App;
